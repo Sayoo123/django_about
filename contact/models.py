c4f84cf7e054a552d05484d77392928319913797
Sayoo123 <sayoojsunil2000@gmail.com>
2023-06-09 01:43:49 +0530

    3rd commit

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=50)
+    subject = models.CharField(max_length=100)
+    message = models.CharField(max_length=1000)
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,16 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
+from .models import contact
+
 
 # Create your views here.
-def contact(request):
-    return render(request,"indexnew.html")
+def contactnew(request):
+      if request.method == 'POST':
+                name = request.POST.get('name')
+                email = request.POST.get('email')
+                subject = request.POST.get('subject')
+                message = request.POST.get('message')
+                contact_data = contact(name = name, email=email, subject=subject,message=message)
+                contact_data.save()
+                return render(request, "indexnew.html")  
+      return render(request,"indexnew.html")
